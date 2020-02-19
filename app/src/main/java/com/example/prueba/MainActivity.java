db893480f5c4876eb4710100669cf06007f5c5b1
sergio Varela <sergioalberto.varelab@gmail.com>
2020-02-19 04:45:43 -0700

    se incorpora funcionalidad de camara y boton (bien feo) de login

--- app/src/main/java/com/example/prueba/MainActivity.java
+++ app/src/main/java/com/example/prueba/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.prueba;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView iv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iv = findViewById(R.id.iv);
+        Intent camara  = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(camara,0);
+        Button btnLog = findViewById(R.id.buttonLogin);
+        btnLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap imagen = (Bitmap)data.getExtras().get("data");
+
+        iv.setImageBitmap(imagen);
+
+    }
+
+    public void login(){
+        Intent intento = new Intent(getApplicationContext(),Login.class);
+        startActivity(intento);
+    }
+
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
     }
 }
